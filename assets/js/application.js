dbcf84447d3971fe85ab564772ca214230706312
Emil Karlsson <emil@nocweb.se>
2012-12-10 00:14:01 +0100

    Adding new content

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.nav-toggle:visible').click(function(){
+  $('.nav-toggle:visible').on('click',function(){
     $('.hf-wrapper header').toggleClass('nav-active');
   });
 });
