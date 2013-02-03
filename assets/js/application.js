0c196bae9e4dd6b2c8980d0d86d416cf478daa7b
Emil Karlsson <emil@nocweb.se>
2013-02-03 22:06:15 +0100

    Added Disqus comments

--- assets/js/application.js
+++ assets/js/application.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
     $('.bucket-items-completed').html($('article li.complete').length);
   }
 });
+
+var el = document.getElementById('disqus_show');
+var disqusShowing = false;
+var disqusShortname = 'skrivremilkarl';
+var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+dsq.src = 'http://' + disqusShortname + '.disqus.com/embed.js';
+
+function expandDisqus() {
+	disqusShowing = true;
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	el.parentNode.removeChild(el);
+}
+
+if(el !== undefined && el !== null) {
+	el.addEventListener('click', expandDisqus, false);
+}
+
+window.onscroll = function () {
+	if (! disqusShowing && (window.innerHeight + window.scrollY) >= (document.height - 300)) {
+		expandDisqus();
+	}
+};
